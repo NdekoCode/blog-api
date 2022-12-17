b480dd6c2174e2a07e6794f32b5709d947cf35f4
Ndekocode <arickbulakali@gmail.com>
2022-12-17 20:16:31 +0200

    feat:get posts

--- controllers/PostsCTRL.js
+++ controllers/PostsCTRL.js
@@ -1,11 +1,16 @@
 import PostMDL from "../models/PostMDL.js";
 import Alert from "../utils/Alert.js";
+import { isEmpty } from "../utils/validators.js";
 
 export default class PostsCTRL {
   async getPosts(req, res) {
     try {
       const posts = await PostMDL.find({}).sort({ createdAt: -1 }).exec();
-      return res.json({ posts });
+      if (!isEmpty(posts)) {
+        return res.json({ posts });
+      }
+
+      return res.json({ posts: [] });
     } catch (error) {
       const alert = new Alert(req, res);
       return alert.danger(error.message, 500);
