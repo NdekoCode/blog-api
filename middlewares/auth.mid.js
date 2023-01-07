16b019a6b20c766299ea23f6702b4d4a9ee8a1a5
Ndekocode <arickbulakali@gmail.com>
2023-01-07 23:45:08 +0200

    feat: add auth middleware

--- middlewares/auth.mid.js
+++ middlewares/auth.mid.js
@@ -1,2 +1,17 @@
-// import jwt from 'jsonwebtoken';
-const auth = (req, res, next) => {};
+import { verify } from "jsonwebtoken";
+import Alert from "../utils/Alert";
+export const auth = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodeToken = verify(token, process.env.SECRET_KEY || "RANDOM");
+    const { userId, email } = decodeToken;
+    req.auth = { userId, email };
+    next();
+  } catch (error) {
+    const alert = new Alert(req, res);
+    return alert.danger(
+      "Vous devez etre connecté pour acceder à cette page",
+      401
+    );
+  }
+};
