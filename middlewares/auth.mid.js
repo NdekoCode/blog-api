8ab23d145092b505b12ffed17c734f9deb543b48
Ndekocode <arickbulakali@gmail.com>
2023-01-07 23:56:27 +0200

    feat: add auth middleware to main routes

--- middlewares/auth.mid.js
+++ middlewares/auth.mid.js
@@ -1,17 +1,14 @@
-import { verify } from "jsonwebtoken";
-import Alert from "../utils/Alert";
+import jwt from "jsonwebtoken";
+import Alert from "../utils/Alert.js";
 export const auth = (req, res, next) => {
+  const alert = new Alert(req, res);
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodeToken = verify(token, process.env.SECRET_KEY || "RANDOM");
+    const decodeToken = jwt.verify(token, process.env.SECRET_KEY || "RANDOM");
     const { userId, email } = decodeToken;
     req.auth = { userId, email };
     next();
   } catch (error) {
-    const alert = new Alert(req, res);
-    return alert.danger(
-      "Vous devez etre connecté pour acceder à cette page",
-      401
-    );
+    alert.danger("Vous devez etre connecté pour acceder à cette page ", 401);
   }
 };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import UsersCTRL from "../controllers/UsersCTRL.js";
+import { auth } from "../middlewares/auth.mid.js";
 const usersRoutes = Router();
 const users = new UsersCTRL();
-usersRoutes.get("/users", users.getUsers);
-usersRoutes.get("/users/:id", users.getUser);
 usersRoutes.post("/users/add", users.signin);
 usersRoutes.post("/login", users.login);
-usersRoutes.put("/users/update/:id", users.updateUser);
-usersRoutes.delete("/users/delete/:id", users.deleteUser);
+usersRoutes.get("/users", auth, users.getUsers);
+usersRoutes.get("/users/:id", auth, users.getUser);
+usersRoutes.put("/users/update/:id", auth, users.updateUser);
+usersRoutes.delete("/users/delete/:id", auth, users.deleteUser);
 export default usersRoutes;
